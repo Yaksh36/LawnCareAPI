39b32a0e64811e2cb9fec2bc16b8e13adbf3b778
Yaksh Patel <yaksh3636@gmail.com>
2021-01-14 18:41:21 -0700

    Initial Commit

---
+++ src/main/java/patel/yaksh/lawn/LawnCareApplication.java
@@ -0,0 +1,13 @@
+package patel.yaksh.lawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawnCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawnCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/patel/yaksh/lawn/LawnCareApplicationTests.java
@@ -0,0 +1,13 @@
+package patel.yaksh.lawn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawnCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
