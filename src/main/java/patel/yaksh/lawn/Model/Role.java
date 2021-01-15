c6cdb342b421bf9ceda2b5a998da4b68a67abf92
Yaksh Patel <yaksh3636@gmail.com>
2021-01-14 18:41:04 -0700

    Initial Commit

---
+++ src/main/java/patel/yaksh/lawn/Model/Role.java
@@ -0,0 +1,5 @@
+package patel.yaksh.lawn.Model;
+
+public class Role {
+    private int id;
+}
---
+++ src/main/java/patel/yaksh/lawn/Model/User.java
@@ -0,0 +1,13 @@
+package patel.yaksh.lawn.Model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    public int id;
+
+}
